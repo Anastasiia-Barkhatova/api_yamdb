83f891e928d9396a917bb5ab66d80f3892828aa3
Maksim Karaskin <mac7sim@gmail.com>
2024-07-02 17:39:15 +0300

    Сериализаторы плюс начал представления

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from reviews.models import Category
+
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class CategoryViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = TitleSerializer
