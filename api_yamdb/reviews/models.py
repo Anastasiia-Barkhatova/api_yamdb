27544dba5b2cb1631570b77516757d486c6d9a4d
Maksim Karaskin <mac7sim@gmail.com>
2024-07-02 14:37:22 +0300

    Сделал модели Category, Genre, Title.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Заголовок',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Slug категории',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        verbose_name='Жанр',
+        max_length=256
+    )
+    slug = models.SlugField(
+        verbose_name='Slug жанра',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'жанр'
+        verbose_name_plural = 'жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        verbose_name='Произведение',
+        max_length=256,
+    )
+    year = models.SmallIntegerField(
+        verbose_name='Год выпуска',
+        db_index=True
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='categoryes'
+    )
+    genres = models.ManyToManyField(Genre)
+
+    class Meta:
+        verbose_name = 'произведение'
+        verbose_name_plural = 'произведения'
+
+    def __str__(self):
+        return self.name
