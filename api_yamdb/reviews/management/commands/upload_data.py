69ff9e0a051d45d8448d65334b852f29503c0531
Maksim Karaskin <mac7sim@gmail.com>
2024-07-04 15:43:03 +0300

    Загрузка данных в базу доработки

--- api_yamdb/reviews/management/commands/upload_data.py
+++ api_yamdb/reviews/management/commands/upload_data.py
@@ -10,31 +10,52 @@ from reviews.models import Category, Comment, Genre, Review, Title
 class Command(BaseCommand):
     help = 'Залить данные из файлов csv в базу данных'
     data = {
-        'category.csv': Category,
-        'comments.csv': Comment,
-        'genre_title.csv': Title,
-        'genre.csv': Genre,
-        'review.csv': Review,
-        'titles.csv': Title,
-        'users.csv': Genre,
+        'category': Category,
+        'comments': Comment,
+        'genre_title': Genre_Title,
+        'genre': Genre,
+        'review': Review,
+        'titles': Title,
+        'users': Genre,
     }
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file_name',
+            type=str,
+            help=(
+                'Укажите имя файла который необходимо загрузить в базу.'
+                'Для загрузки всех файлов из директории, укажите - "all"'
+            )
+        )
+
+    def handle(self, *args, **options):
+        file_name = options['file_name']
         try:
+            if file_name != 'all':
+                self.data = {file_name: self.data[file_name]}
             for file_name, model in self.data.items():
                 try:
                     file_csv = open(
-                        f'static/data/{file_name}', encoding='UTF-8'
+                        f'static/data/{file_name}.csv', encoding='UTF-8'
                     )
                 except OSError:
-                    print('Could not open/read file:', file_name)
+                    self.stdout.write(
+                        self.style.ERROR(
+                            'Could not open/read file:', file_name
+                        )
+                    )
                     sys.exit()
                 with file_csv:
                     reader = csv.DictReader(file_csv)
                     for row in reader:
-                        print(row)
                         model.objects.create(**row)
-                        file_csv.close()
-
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'Данные из файла {file_name}.csv внесены в базу'
+                        )
+                    )
         except DatabaseError as error:
-            print(error)
+            self.stdout.write(self.style.ERROR(f'Ошибка базы данных {error}'))
+        except Exception as error:
+            self.stdout.write(self.style.ERROR(error))
