7477967694c4c15209193d541930eafd92463894
Anastasiia-Barkhatova <155043139+Anastasiia-Barkhatova@users.noreply.github.com>
2024-07-10 07:54:39 +0300

    Update serializers.py - изменен TitleSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -10,14 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = '__all__'
+        exclude = ('id',)
 
 
 class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Genre
-        fields = '__all__'
+        exclude = ('id',)
 
 
 class TitleSerializerList(serializers.ModelSerializer):
